6eb5f8698f487adc9bb93d6dfa75262f87fbeb67
Tine Skoberne <tine.skoberne@gmail.com>
2022-12-01 22:46:49 +0100

    Exercises 3.1-3.6: phonebook backend

---
+++ part3/phonebook/index.js
@@ -0,0 +1,98 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello phonebook</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    }
+    else {
+        response.status(404).end()
+    }
+})
+
+const generateId = () => {
+    return Math.round(Math.random()*1000000)
+}
+
+const errorResponse = (response, errorMsg) => {
+    return response.status(400).json({
+        error: errorMsg
+    })
+}
+
+app.post('/api/persons/', (request, response) => {
+    const body = request.body
+
+    if (!body.name) {
+        return errorResponse(response, 'name missing')
+    }
+    if (!body.number) {
+        return errorResponse(response, 'number missing')
+    }
+    if (persons.find(person => person.name === body.name)) {
+        return errorResponse(response, 'name must be unique')
+    }
+
+    const newPerson = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+    persons = persons.concat(newPerson)
+
+    response.json(newPerson)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+  
+    response.status(204).end()
+})
+
+app.get('/api/info', (request, response) => {
+    const date = new Date()
+    let msg = `<p>Phonebook has info for ${persons.length} people.</p>`
+    msg += `<p>${date}</p>`
+    response.send(msg)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
